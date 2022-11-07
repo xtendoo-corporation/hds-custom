67c9de1a88c29c37af28e0d4ca06c908d4f950c1
dani <dani.xtendoo@gmail.com>
2022-11-07 11:57:10 +0100

    sync

---
+++ document_format_HDS/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Document Format HDS",
+    "summary": """Formatos de documentos HDS""",
+    "version": "13.0.1.0.0",
+    "description": """Formatos de documentos HDS""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/layout/external_layout_clean.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
