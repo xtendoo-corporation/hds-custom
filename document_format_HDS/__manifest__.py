c3f1da964a8a0d2fbcdad2ce5470bb0f320d8963
dani <dani.xtendoo@gmail.com>
2022-11-08 08:43:39 +0100

    sync

--- document_format_HDS/__manifest__.py
+++ document_format_HDS/__manifest__.py
@@ -13,6 +13,9 @@
     ],
     "data": [
         "views/layout/external_layout_clean.xml",
+        "views/sale/sale_order_document.xml",
+        "views/invoice/invoice_document.xml",
+        "views/delivery/delivery_document.xml",
     ],
     "installable": True,
     "auto_install": False,
